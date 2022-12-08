ea836f3cb9eb6f013d4d9aae86ea5c79791c6c8e
Mohammad Khalatbari <khalatbari.computer@gmail.com>
2022-12-08 19:31:16 +0200

    first commit

--- DisariumNumber.py
+++ DisariumNumber.py
@@ -1,8 +1,16 @@
+# A number is said to be the Disarium number when the sum of its digit raised to the power of their respective positions becomes equal to the number itself.
+#
+# For example, 175 is a Disarium number as follows:
+#
+# 11+ 72 + 53 = 1+ 49 + 125 = 175
+
+
 def power(n, power):
     temp = n * n
     for i in range(power - 2):
         temp = temp * n
     return temp
+
 
 inputNumber = input("Please enter your number: ")
 numbList = []
